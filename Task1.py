beae5a7c7324dcfb3f56dec14ee374e6bacaf46d
Ekaterina <94058290+rytee20@users.noreply.github.com>
2022-10-06 16:23:04 +0700

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -1,13 +1,13 @@
-from random import randint
+import ipaddress
+import random
+
+MAX_IPV4 = ipaddress.IPv4Address._ALL_ONES  # 2 ** 32 - 1
 
 def generator(amount):
     for n in range(amount):
-        a = randint(0,255)
-        b = randint(0,255)
-        c = randint(0,255)
-        d = randint(0,255)
+        ip = ipaddress.IPv4Address._string_from_ip_int(random.randint(0, MAX_IPV4))
         f = open('ip.log', 'a', encoding='utf-8')
-        f.write(str(a)+'.'+str(b)+'.'+str(c)+'.'+str(d)+'\n')
+        f.write(str(ip)+'\n')
         f.close()
     print('Адреса сгенерированы!')
 

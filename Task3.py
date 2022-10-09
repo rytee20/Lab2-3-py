43fa5bcd5fdf00e7099ff7fabe7b4a8adabab2f7
Ekaterina <94058290+rytee20@users.noreply.github.com>
2022-10-09 17:27:49 +0700

    Update Task3.py

--- Task3.py
+++ Task3.py
@@ -1,8 +1,7 @@
-from operator import itemgetter
+import csv
 
-
-def sort_key(e): #ключ для сортировки
-    return e[1]
+def sort_key(key): #ключ для сортировки
+    return key['Количество побед']
 
 def printList(flist): #функция для печати списка
     for i in range(0, len(flist)):
@@ -11,50 +10,55 @@ def printList(flist): #функция для печати списка
 def sortByThird(fparticipants): #сортировка по дополнительным баллам методом камня
     for i in range(len(fparticipants)-1):
         for j in range(len(fparticipants)-i-1):
-            if fparticipants[j][1]==fparticipants[j+1][1] and fparticipants[j][2] < fparticipants[j + 1][2]:
+            if fparticipants[j]['Количество побед']==fparticipants[j+1]['Количество побед'] and fparticipants[j]['Доп. показатель'] < fparticipants[j + 1]['Доп. показатель']:
                 fparticipants[j], fparticipants[j + 1] = fparticipants[j + 1], fparticipants[j]
     return fparticipants
 
 
 participants=[] #участники
+winners=[] #результаты
 heading='' #заголовок
 endoffile='no' #конец строки
 raiting=1 #призовые места
 
-with open ('players.csv', 'r', encoding='utf-8') as f: #открываем файл
-    heading=f.readline() #считываем заголовок
-    while endoffile!='':
-        endoffile=f.readline()#считываем участника
-        if endoffile=='': #как только пустая строка
-            break #выходим из цикла
-        participants.append(list(map(str,endoffile.split(';')))) #записываем в список
-f.close()
+
+with open('players.csv', encoding='utf-8') as r_file: #считываем содеримое файла
+    file_reader = csv.DictReader(r_file, delimiter = ";")
+    participants=list(file_reader) #преобразовываем в лист
 
 printList(participants) #печатаем участников для проверки
 
-for i in range(0,len(participants)): #преобразоваваем победы и дополнительные баллы в int из str
-    participants[i][1]=int(participants[i][1])
-    participants[i][2]=int(participants[i][2])
+#for i in range(0,len(participants)): #преобразоваваем победы и дополнительные баллы в int из str
+ #   participants[i]['Количество побед']=int(participants[i]['Количество побед'])
+ #   participants[i]['Доп. показатель']=int(participants[i]['Доп. показатель'])
 
 participants.sort(key=sort_key,reverse=True) #сортируем по победам
 printList(participants) #вывод для проверки
 participants=sortByThird(participants) #сортируем по дополнительным очкам
-printList(participants) #вывод для проверки 
+printList(participants) #вывод для проверки
 
-with open('results.csv', 'a', encoding='utf-8') as f: #записываем результаты в файл
-    f.write('Спортсмен;Место\n') #заголовок
-    for i in range(len(participants)):
-        if i==0: # если участник первый в списке
-            f.write(str(participants[i][0]) + ';' + str(raiting) + '\n') #то записываем его на первное место
+for i in range(len(participants)): #формеруем список результатов
+    if i == 0:  # если участник первый в списке
+        winners.append({'Спортсмен': participants[i]['Спортсмен'], 'Место': raiting}) # то записываем его на первное место
+    else:
+        if participants[i]['Количество побед'] == participants[i - 1]['Количество побед'] and participants[i]['Доп. показатель'] == participants[i - 1][
+            'Доп. показатель']:  # если у текущего участника и предыдущго совпадают победы и доп баллы
+            winners.append({'Спортсмен': participants[i]['Спортсмен'], 'Место': raiting})  # то записываем текущего участника с таким же местом как у предыдущего
         else:
-            if participants[i][1]==participants[i-1][1] and participants[i][2]==participants[i-1][2]: #если у текущего участника и предыдущго совпадают победы и доп баллы
-                f.write(str(participants[i][0]) + ';' + str(raiting) + '\n') #то записываем текущего участника с таким же местом как у предыдущего
-            else:
-                if participants[i][1]==participants[i-1][1] and participants[i][2]!=participants[i-1][2]: #если у учатника текущего и предыдущего совпадают победы но разные доп баллы
-                    raiting=raiting+1 #прибавляем место
-                    f.write(str(participants[i][0]) + ';' + str(raiting) + '\n') #записываем его со следующим местом
-                else: #если вообще ничего не совпадает
-                    raiting = raiting + 1 #прибавляем место
-                    f.write(str(participants[i][0]) + ';' + str(raiting) + '\n') #и записываем
-    f.close()
+            if participants[i]['Количество побед'] == participants[i - 1]['Количество побед'] and participants[i]['Доп. показатель'] != participants[i - 1][
+                'Доп. показатель']:  # если у учатника текущего и предыдущего совпадают победы но разные доп баллы
+                raiting=raiting+1  # прибавляем место
+                winners.append({'Спортсмен': participants[i]['Спортсмен'], 'Место': raiting})  # записываем его со следующим местом
+            else:  # если вообще ничего не совпадает
+                raiting=raiting+1
+                winners.append({'Спортсмен': participants[i]['Спортсмен'], 'Место': raiting})  # и записываем
+
+printList(winners) #печатаем для проверки
+
+details = ['Спортсмен', 'Место'] #заголовок
+with open("results.csv", "w", newline="") as f: #записываем результаты
+    write = csv.DictWriter(f, fieldnames=details, delimiter = ";")
+    write.writeheader()
+    write.writerows(winners)
+
 print('Результаты совервнований записаны в файл!')

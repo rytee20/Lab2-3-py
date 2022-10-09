e59b2159086d86da72e684a93c51b9d2e636fe00
Ekaterina <94058290+rytee20@users.noreply.github.com>
2022-10-09 18:29:55 +0700

    Update Task4.py

--- Task4.py
+++ Task4.py
@@ -3,6 +3,6 @@ import random
 
 os.mkdir("example")
 for i in range(0,100):
-    file = open("example/examplefile.txt"+str(i), "w")
+    file = open("example/examplefile"+str(i), "w")
     size = 1024*random.randint(1, 100)
     file.write(" " * size)
